8dd7a66ca2bb2662ef71fb9429edebb41d2b4cca
Gil Hildebrand <gilhildebrand@gmail.com>
2018-09-19 21:34:18 -0500

    Begin Request Network integration

--- src/traveler/requestnetwork.ts
+++ src/traveler/requestnetwork.ts
@@ -1,13 +1,66 @@
 import { Traveler, Network } from "../lib/shared";
 import { ITransaction } from "../lib/transaction";
 import { IInvoice } from "../lib/invoice";
+// import RequestNetworkDataFormat from "requestnetwork-data-format";
 
 export interface IRequestNetworkTransaction {
-  // define the fields in this transaction type
+  // not used by Request Network
 }
 
 export interface IRequestNetworkInvoice {
-  // define the fields in this invoice type
+  // details at https://github.com/RequestNetwork/requestNetwork/tree/master/packages/requestNetworkDataFormat/src/format/rnf_invoice
+  
+  meta: {
+    format: "rnf_invoice";
+    version: "0.0.1";
+  };
+  creationDate: Date;
+  invoiceNumber: string;
+  purchaseOrderId: string;
+  note: string;
+  terms: string;
+  sellerInfo: {
+    email: string;
+    firstName: string;
+    lastName: string;
+    businessName: string;
+    phone: string;
+    address: {};
+    taxRegistration: string;
+    companyRegistration: string;
+    miscellaneous: {};
+  };
+  buyerInfo: {
+    email: string;
+    firstName: string;
+    lastName: string;
+    businessName: string;
+    phone: string;
+    address: {};
+    taxRegistration: string;
+    companyRegistration: string;
+    miscellaneous: {};
+  };
+  invoiceItems: [
+    {
+      name: string;
+      reference: string;
+      quantity: number;
+      unitPrice: number;
+      discount: number;
+      taxPercent: number;
+      currency: string;
+      deliberyDate: Date;
+      deliveryPeriod: string;
+    }
+  ];
+  paymentTerms: {
+    dueDate: Date;
+    lateFeesPercent: number;
+    lateFeesFix: number;
+    miscellaneous: {}
+  };
+  miscellaneous: {};
 }
 
 export class RequestNetworkTraveler implements Traveler {
@@ -21,10 +74,16 @@ export class RequestNetworkTraveler implements Traveler {
   }
 
   convertInvoiceTo(invoice: IInvoice) {
-
+    
   }
 
   convertInvoiceFrom(invoice: IRequestNetworkInvoice) {
+    // validate
+    // const validated = RequestNetworkDataFormat.validate(invoice);
+    // if (!validated) {
+    //   // console.log(validated.errors)
+    //   throw new Error('Request Network: Invalid invoice format');
+    // }
 
   }
 
