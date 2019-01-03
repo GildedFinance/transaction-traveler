76af32918d729aa5a681ed729c6bcafbe76bc49d
Eledi Dyrkaj <edyrkaj@gmail.com>
2019-01-03 19:08:52 +0100

    working on request network invoice transform

--- src/lib/invoice.ts
+++ src/lib/invoice.ts
@@ -1,3 +1,50 @@
 export interface IInvoice {
-  id: string;
+  ref?: string;
+  invoice_number: number;
+  user_id: string;
+  client_id: string; // assigned from firebase
+  title: string;
+  items: InvoiceItem[];
+  terms: string;
+  total_amount: number;
+  discount: number;
+  currency: string;
+  fiat_currency: string;
+  receive_currency: string;
+  notes: string;
+  status: string;
+
+  // date
+  created_at: any;
+  updated_at: any;
+  deleted_at?: any;
+  date_due: any; // timestamp
+  date_send: any; // timestamp
+  date_paid: any; // timestamp
+
+  // transactions info
+  transaction_id?: any;
+  transaction_network?: string;
+  transaction_network_id?: number;
+  transaction_data?: any;
+
+  invoice_method: string; // address | requestnetwork
+  payment_type?: string; // request | send
+
+  exchange_currency?: string;
+  exchange_value?: number;
+  exchange_rate?: number;
+  exchange_date?: any;
+
+  requestId?: string;
+  creator?: string;
+  payer?: string;
+}
+
+export interface InvoiceItem {
+  id?: number;
+  description: string;
+  quantity: number;
+  amount: number;
+  unit_price: number;
 }
--- src/traveler/requestnetwork.ts
+++ src/traveler/requestnetwork.ts
@@ -6,13 +6,59 @@ export interface IRequestNetworkTransaction {
   // define the fields in this transaction type
 }
 
+export interface IRequestNetworkAccountInfo {
+  email: string;
+  firstName: string;
+  lastName: string;
+  businessName: string;
+  phone: string;
+  address: string;
+  taxRegistration: string;
+  companyRegistration: string;
+  miscellaneous: any;
+}
+
+export interface IRequestNetworkInvoicePaymentTerms {
+  dueDate: string;
+  lateFeesPercent: number;
+  lateFeesFix: string;
+  miscellaneous: any;
+}
+
+export interface IRequestNetworkInvoiceItem {
+  name: string;
+  quantity: number;
+  unitPrice: string;
+  taxPercent: number;
+  currency: string;
+}
+
 export interface IRequestNetworkInvoice {
+  // tslint:disable-next-line:max-line-length
+  // https://github.com/RequestNetwork/requestNetwork/blob/master/packages/requestNetworkDataFormat/src/format/rnf_invoice/rnf_invoice-0.0.2.json
   // define the fields in this invoice type
+  meta: {
+    format: 'rnf_invoice',
+    version: '0.0.2'
+  };
+  invoiceNumber: string;
+  creationDate: string;
+  invoiceItems: [];
+  // optional
+  purchaseOrderId?: string;
+  note?: string;
+  terms?: string;
+  sellerInfo?: IRequestNetworkAccountInfo;
+  buyerInfo?: IRequestNetworkAccountInfo;
+  paymentTerms?: IRequestNetworkInvoicePaymentTerms;
+  miscellaneous?: any;
 }
 
 export class RequestNetworkTraveler implements Traveler {
 
-  convertTransactionTo(txn: ITransaction) {}
+  convertTransactionTo(txn: ITransaction) {
+    //
+  }
 
   convertTransactionFrom(txn: IRequestNetworkTransaction) {}
 
